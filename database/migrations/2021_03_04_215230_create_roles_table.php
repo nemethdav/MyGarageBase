5d92bbeccbbe85a3e6f4fe7bb69a794442fd6b1e
nemethdav <60889598+nemethdav@users.noreply.github.com>
2021-03-13 15:57:42 +0100

    Migrációk és adatbázis módosítása

--- database/migrations/2021_03_04_215230_create_roles_table.php
+++ database/migrations/2021_03_04_215230_create_roles_table.php
@@ -15,8 +15,6 @@ class CreateRolesTable extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
             $table->text('role');
             $table->timestamps();
         });
