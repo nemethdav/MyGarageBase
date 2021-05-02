92d31432b547995ad3ea2f06b10d1659e126e514
nemethdav <60889598+nemethdav@users.noreply.github.com>
2021-05-02 17:13:25 +0200

    Tankolás backend elkészítése

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RefuelingController;
 use App\Http\Controllers\VehicleController;
 use Illuminate\Support\Facades\Route;
 
