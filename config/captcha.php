0a702e3faab6e008cf4d880dc39a8297a1dcc629
nemethdav <60889598+nemethdav@users.noreply.github.com>
2021-10-02 11:10:23 +0200

    reCAPTCHA adatok a captcha.php konfigurációs fájlba

--- config/captcha.php
+++ config/captcha.php
@@ -1,8 +1,10 @@
 <?php
 
 return [
-    'secret' => env('NOCAPTCHA_SECRET'),
-    'sitekey' => env('NOCAPTCHA_SITEKEY'),
+//    'secret' => env('NOCAPTCHA_SECRET'),
+//    'sitekey' => env('NOCAPTCHA_SITEKEY'),
+    'secret' => '6LfBb6McAAAAAA0YZLpjgg741ljq1p--oJwQo0a3',
+    'sitekey' => '6LfBb6McAAAAAKhIOj0IikDfF5-jLMiQUxH-wstB',
     'options' => [
         'timeout' => 30,
     ],
