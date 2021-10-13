0faa03a8fb0b393bd342b4027e3778a479eb2039
nemethdav <60889598+nemethdav@users.noreply.github.com>
2021-10-13 09:06:27 +0200

    Hibajavítás - E-mail kliensoldali validáció engedélyezés

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -5,7 +5,7 @@
 <div class="container" style="height: auto;">
   <div class="row align-items-center">
     <div class="col-lg-4 col-md-6 col-sm-8 ml-auto mr-auto">
-      <form novalidate class="form" method="POST" action="{{ route('password.email') }}">
+      <form class="form" method="POST" action="{{ route('password.email') }}">
         @csrf
 
         <div class="card card-login card-hidden mb-3">
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -5,7 +5,7 @@
 <div class="container" style="height: auto;">
   <div class="row align-items-center">
     <div class="col-lg-4 col-md-6 col-sm-8 ml-auto mr-auto">
-      <form novalidate class="form" method="POST" action="{{ route('password.update') }}">
+      <form class="form" method="POST" action="{{ route('password.update') }}">
         @csrf
 
         <input type="hidden" name="token" value="{{ $token }}">
