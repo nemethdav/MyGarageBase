49cc62f67e1cbdd4385284c222ae371a4ec3ffa3
nemethdav <60889598+nemethdav@users.noreply.github.com>
2021-11-10 19:26:50 +0100

    Roles DB seeder

---
+++ database/seeders/RolesTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->insert([
+            'role' => 'Felhasználó',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('roles')->insert([
+            'role' => 'Adminisztrátor',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+    }
+}
